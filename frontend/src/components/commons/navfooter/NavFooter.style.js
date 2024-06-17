d80e5068797637c73c57703cc21ac0643a8d228b
uttamapaksa <uttamapaksa@gmail.com>
2024-06-17 15:18:39 +0900

    feat: 헤더와 푸터의 디자인 및 라우팅 KAN-119

---
+++ frontend/src/components/commons/navfooter/NavFooter.style.js
@@ -0,0 +1,43 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+    height: 80px;
+    background-color: black;
+    color: white;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 1.2rem;
+`;
+
+export const Section = styled.div`
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
+`;
+
+export const SectionImage = styled.img`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 30%;
+    // width: 100%;
+`;
+
+export const SectionText = styled.div`
+    margin-top: 3%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 30%;
+    width: 100%;
+    font-size: 13px;
+    font-weight: 600;
+`;
