9e397c1e7fd8c6d90049413be005e13d231b12b7
김진규 <swc05161@gmail.com>
2024-06-18 14:19:01 +0900

    Revert "feat: KAN-58 코스 상세조회 및 조회"

--- backend/src/main/java/com/hyundairoad/hyundairoad/course/service/CourseService.java
+++
@@ -1,9 +0,0 @@
-package com.hyundairoad.hyundairoad.course.service;
-
-import com.hyundairoad.hyundairoad.course.domain.dto.CourseDetailDto;
-import java.util.List;
-
-public interface CourseService {
-    List<CourseDetailDto> getAllCourses(Long memberId);
-    CourseDetailDto getCourseDetailsById(Long id, Long memberId);
-}
