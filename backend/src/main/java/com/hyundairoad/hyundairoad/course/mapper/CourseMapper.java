a44736bfd9d702d2457a393a6f3a755eac345c5f
wlstnam <wlstnam99@gmail.com>
2024-06-18 17:03:16 +0900

    feat : KAN-59 코스 생성

--- backend/src/main/java/com/hyundairoad/hyundairoad/course/mapper/CourseMapper.java
+++ backend/src/main/java/com/hyundairoad/hyundairoad/course/mapper/CourseMapper.java
@@ -1,6 +1,8 @@
 package com.hyundairoad.hyundairoad.course.mapper;
 
 import com.hyundairoad.hyundairoad.course.domain.dto.CourseDetailDto;
+import com.hyundairoad.hyundairoad.course.domain.dto.CreateCourseDto;
+import com.hyundairoad.hyundairoad.course.domain.dto.CreateCoursePlaceDto;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import java.util.List;
@@ -9,4 +11,8 @@ import java.util.List;
 public interface CourseMapper {
     List<CourseDetailDto> getAllCourses(Long memberId);
     CourseDetailDto getCourseDetailsById(@Param("courseId") Long courseId, @Param("memberId") Long memberId);
+
+    void insertCourse(CreateCourseDto createCourseDto);
+
+    void insertCoursePlace(CreateCoursePlaceDto createCoursePlaceDto);
 }
--- backend/src/main/java/com/hyundairoad/hyundairoad/course/service/CourseServiceImpl.java
+++ backend/src/main/java/com/hyundairoad/hyundairoad/course/service/CourseServiceImpl.java
@@ -1,9 +1,13 @@
 package com.hyundairoad.hyundairoad.course.service;
 
 import com.hyundairoad.hyundairoad.course.domain.dto.CourseDetailDto;
+import com.hyundairoad.hyundairoad.course.domain.dto.CreateCoursePlaceDto;
 import com.hyundairoad.hyundairoad.course.mapper.CourseMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import com.hyundairoad.hyundairoad.course.domain.dto.CreateCourseDto;
+
 import java.util.List;
 
 @Service
@@ -11,6 +15,22 @@ import java.util.List;
 public class CourseServiceImpl implements CourseService {
 
     private final CourseMapper courseMapper;
+
+    @Transactional
+    public void createCourse(CreateCourseDto createCourseDto) {
+        courseMapper.insertCourse(createCourseDto);
+        Long courseId = createCourseDto.getCourseId();
+        createCourseDto.getCoursePlaces().forEach(place -> {
+            CreateCoursePlaceDto createCoursePlaceDto = CreateCoursePlaceDto.builder()
+                    .courseId(courseId)
+                    .placeId(place.getPlaceId())
+                    .memo(place.getMemo())
+                    .rank(place.getRank())
+                    .image(place.getImage())
+                    .build();
+            courseMapper.insertCoursePlace(createCoursePlaceDto);
+        });
+    }
 
     @Override
     public CourseDetailDto getCourseDetailsById(Long courseId, Long memberId) {
