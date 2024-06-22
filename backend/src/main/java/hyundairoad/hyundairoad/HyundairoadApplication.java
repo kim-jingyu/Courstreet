a0459049d2673c7476ac2aeab25784d966ad5ea8
kim-jingyu <swc05161@gmail.com>
2024-06-22 15:03:38 +0900

    init

---
+++ backend/src/main/java/hyundairoad/hyundairoad/HyundairoadApplication.java
@@ -0,0 +1,13 @@
+package hyundairoad.hyundairoad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HyundairoadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HyundairoadApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/hyundairoad/hyundairoad/HyundairoadApplicationTests.java
@@ -0,0 +1,13 @@
+package hyundairoad.hyundairoad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HyundairoadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
